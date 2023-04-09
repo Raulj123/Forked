50665f8836058087c6fdb8a751873e4b0ebc42cd
raul <jarquinr121@gmail.com>
2023-04-09 10:56:56 -0700

    added new colors and started code for users profile

---
+++ profile.js
@@ -0,0 +1,11 @@
+var firstName = document.querySelector("#firstname");
+var lastName = document.querySelector("#lastname");
+var email = document.querySelector("#email");
+var major = document.querySelector("#major");
+var subBtn = document.querySelector("#sub");
+
+subBtn.addEventListener('click', addData);
+
+function addData(){
+    console.log(major);
+}
